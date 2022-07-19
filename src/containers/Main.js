311e24f3f0de01c05826cb2a44ba71e1a3b62d91
Viet-Tien Ngoc <tiena2cva@gmail.com>
2022-07-20 00:29:23 +0700

    remove unuse variable

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -6,8 +6,7 @@ import Experience from "../pages/experience/Experience";
 import Opensource from "../pages/opensource/Opensource";
 import Contact from "../pages/contact/ContactComponent";
 import Projects from "../pages/projects/Projects";
-import { settings } from "../portfolio.js";
-import Error404 from "../pages/errors/error404/Error";
+// import { settings } from "../portfolio.js";
 
 export default class Main extends Component {
   render() {
--- src/pages/opensource/Opensource.js
+++ src/pages/opensource/Opensource.js
@@ -3,8 +3,8 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import OpensourceCharts from "../../containers/opensourceCharts/OpensourceCharts";
 import Organizations from "../../containers/organizations/Organizations";
-import PullRequests from "../../containers/pullRequests/PullRequests";
-import Issues from "../../containers/issues/Issues";
+// import PullRequests from "../../containers/pullRequests/PullRequests";
+// import Issues from "../../containers/issues/Issues";
 import TopButton from "../../components/topButton/TopButton";
 import "./Opensource.css";
 
